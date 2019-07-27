e345228dba5d6899591accc3f90be033ab256300
Ryan Davis <ryand-ruby@zenspider.com>
2019-07-27 14:41:47 -0700

    Initial skeleton

---
+++ lib/assert_scanner.rb
@@ -0,0 +1,3 @@
+class AssertScanner
+  VERSION = "1.0.0"
+end
---
+++ test/test_assert_scanner.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "assert_scanner"
+
+class TestAssertScanner < Minitest::Test
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
